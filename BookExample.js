15a9ce80710990af1d810ed13d67d69cf5784557
mirajali4321 <alimuhammadmiraj@gmail.com>
2024-04-25 16:26:30 +0500

    complete project

--- BookExample.js
+++ BookExample.js
@@ -1,24 +1,46 @@
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+const myLibrary = [];
 
-  this.status = function () {
-    console.log(
-      "The",
-      this.title,
-      "by ",
-      this.author,
-      ",",
-      this.pages,
-      ",",
-      this.read,
-      "."
-    );
-  };
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const book = {};
+  formData.forEach((value, key) => {
+    book[key] = value;
+  });
+  myLibrary.push(book);
+  event.target.reset();
+  $('#exampleModal').modal('hide');
+
+  displayLibrary();
+
+  console.log("form submitted", myLibrary);
 }
 
-const first = new Book("Al-rahekhul Makhdum" , "maulana safi" , "650 pages" , "not yet fully");
+function displayLibrary(){
+  const tableBody = document.getElementById("bookTableBody");
+  tableBody.innerHTML="";
 
-first.status();
+  myLibrary.forEach((book, index)=>{
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <th scope= "row"> ${index+1}</th>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.pages}</td>
+    <td>${book.status}</td>
+    <td><button class="btn btn-danger btn-sm remove-btn">Remove</button></td>
+    `;
+    row.querySelector(".remove-btn").addEventListener("click", function() {
+      removeBook(index);
+    });
+
+  tableBody.appendChild(row);
+  })
+}
+
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayLibrary();
+}
